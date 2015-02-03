dbac68d80874dbfa1ce8cc95d96ad4ece3a1b29e
JinRiong <zhenlong1992@hotmail.com>
2015-02-03 15:35:42 +0900

    test for upload

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,8 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello,World!");
+		System.out.println("Hello,World!");
 	}
 
 }
